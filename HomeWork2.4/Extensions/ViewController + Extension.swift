0a9581c9f37ac14ca56236590a35c1dbb9f641ba
Paul <pasha.makey@gmail.com>
2023-12-02 12:54:49 +0300

    setGradientView + Extension

--- HomeWork2.4/Extensions/ViewController + Extension.swift
+++ HomeWork2.4/Extensions/ViewController + Extension.swift
@@ -5,4 +5,17 @@
 //  Created by Paul Makey on 2.12.23.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func setGradientView() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor.systemPink.cgColor,
+            UIColor.systemTeal.cgColor,
+            UIColor.systemMint.cgColor
+        ]
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
